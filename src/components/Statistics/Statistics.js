078f0b505a42e80494a9145278be7b733af1070e
DrAnastasiia <dr.anastasiyya0@gmail.com>
2021-02-15 13:50:45 +0200

    create statistics component

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Statistics.module.css';
+
+const Statistics = ({ statisticalData }) => {
+  return (
+    <section className={styles.statistics}>
+      <h2 className={styles.title}>Upload stats</h2>
+
+      <ul className={styles.statslist}>
+        {statisticalData.map(({ id, label, percentage }) => {
+          return (
+            <li className={styles.item} key={id}>
+              <span className={styles.label}>{label}</span>
+              <span className={styles.percentage}>{percentage}%</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  statisticalData: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
+};
+
+export default Statistics;
