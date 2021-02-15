686cadd1577182ad96744c758d10fd721aeab3a0
DrAnastasiia <dr.anastasiyya0@gmail.com>
2021-02-15 13:51:22 +0200

    hook up statistics component into App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import user from '../../data/user.json';
-import Profile from '../Profile/Profile';
+import statisticalData from '../../data/statistical-data.json';
 import Container from '../Container/Container';
+import Profile from '../Profile/Profile';
+import Statistics from '../Statistics/Statistics';
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
           avatar={user.avatar}
           stats={user.stats}
         />
+
+        <Statistics statisticalData={statisticalData} />
       </Container>
     </>
   );
