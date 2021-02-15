569b8b648ce636aabb51c2ba2eb29ad9136ce1f9
DrAnastasiia <dr.anastasiyya0@gmail.com>
2021-02-15 12:40:15 +0200

    add new files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+//Модули
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Компоненты
+import App from './components/App/App';
+
+//Стили
+import 'modern-normalize/modern-normalize.css';
+import './styles.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
