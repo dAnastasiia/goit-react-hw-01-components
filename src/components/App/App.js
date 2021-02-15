e7da588000b5933d738890e1414d619ed8cc25eb
DrAnastasiia <dr.anastasiyya0@gmail.com>
2021-02-15 15:23:29 +0200

    hool up transactions component into App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,12 +4,14 @@ import React from 'react';
 import user from '../../data/user.json';
 import statisticalData from '../../data/statistical-data.json';
 import friends from '../../data/friends.json';
+import transactions from '../../data/transactions.json';
 
 //Компоненты
 import Container from '../Container/Container';
 import Profile from '../Profile/Profile';
 import Statistics from '../Statistics/Statistics';
 import FriendList from '../FriendList/FriendList';
+import TransactionHistory from '../TransactionHistory/TransactionHistory';
 
 const App = () => {
   return (
@@ -26,6 +28,8 @@ const App = () => {
         <Statistics statisticalData={statisticalData} />
 
         <FriendList friends={friends} />
+
+        <TransactionHistory transactions={transactions} />
       </Container>
     </>
   );
