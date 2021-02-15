27562f2ac8dbdbdcc22ee88d427ca71de683d400
DrAnastasiia <dr.anastasiyya0@gmail.com>
2021-02-15 20:49:30 +0200

    remove container component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,6 @@ import friends from '../../data/friends.json';
 import transactions from '../../data/transactions.json';
 
 //Компоненты
-import Container from '../Container/Container';
 import Profile from '../Profile/Profile';
 import Statistics from '../Statistics/Statistics';
 import FriendList from '../FriendList/FriendList';
@@ -16,21 +15,19 @@ import TransactionHistory from '../TransactionHistory/TransactionHistory';
 const App = () => {
   return (
     <>
-      <Container>
-        <Profile
-          name={user.name}
-          tag={user.tag}
-          location={user.location}
-          avatar={user.avatar}
-          stats={user.stats}
-        />
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
 
-        <Statistics statisticalData={statisticalData} />
+      <Statistics statisticalData={statisticalData} />
 
-        <FriendList friends={friends} />
+      <FriendList friends={friends} />
 
-        <TransactionHistory transactions={transactions} />
-      </Container>
+      <TransactionHistory transactions={transactions} />
     </>
   );
 };
