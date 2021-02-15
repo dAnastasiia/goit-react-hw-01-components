c938249717c9c6adda9eea87501030ca1b426fb9
DrAnastasiia <dr.anastasiyya0@gmail.com>
2021-02-15 13:10:52 +0200

    add container component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import user from '../../data/user.json';
 import Profile from '../Profile/Profile';
+import Container from '../Container/Container';
 
 const App = () => {
   return (
-    <Profile
-      name={user.name}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <>
+      <Container>
+        <Profile
+          name={user.name}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </Container>
+    </>
   );
 };
 
