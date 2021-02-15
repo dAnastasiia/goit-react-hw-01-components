c0cf721a341d82f5b935515778e1ddcd21c377a0
DrAnastasiia <dr.anastasiyya0@gmail.com>
2021-02-15 15:03:19 +0200

    hook up friendListItem component into App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+
+//Данные
 import user from '../../data/user.json';
 import statisticalData from '../../data/statistical-data.json';
+import friends from '../../data/friends.json';
+
+//Компоненты
 import Container from '../Container/Container';
 import Profile from '../Profile/Profile';
 import Statistics from '../Statistics/Statistics';
+import FriendList from '../FriendList/FriendList';
 
 const App = () => {
   return (
@@ -18,6 +24,8 @@ const App = () => {
         />
 
         <Statistics statisticalData={statisticalData} />
+
+        <FriendList friends={friends} />
       </Container>
     </>
   );
